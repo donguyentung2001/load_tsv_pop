57b68724f069dbad287c616e2c66741e81be3c21
Tung Do <tdo@haverford.edu>
2021-06-22 12:00:51 -0400

    Test

--- script.py
+++ script.py
@@ -12,11 +12,12 @@ with open(input_file) as fd:
     rd = csv.reader(fd, delimiter="\t", quotechar='"')
     col_index  = None 
     for row in rd:
-        if col_index is None: 
-            col_index = row.index(column_name)
-        else: 
-            if row[col_index] == value:
-                output.append(row[0])
+        #if col_index is None: 
+        #    col_index = row.index(column_name)
+        #else: 
+        #    if row[col_index] == value:
+        #        output.append(row[0])
+        print(row)
 tsv_file.close() 
 
 f= open(output_file,"w+")
